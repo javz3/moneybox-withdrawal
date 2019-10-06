1dd433b2c69994a2eff41125896234bfe16af2a6
javz3 <javz@hotmail.co.uk>
2019-10-07 00:34:40 +0100

    [JAVED]: Added basic unit tests

---
+++ src/Moneybox.Tests/Moneybox.Tests/Services/BalanceServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Moneybox.App.Domain.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Moneybox.Tests.Services
+{
+    class BalanceServiceTest
+    {
+        private Mock<IBalanceService> _balanceService;
+
+        [SetUp]
+        public void _SetUp()
+        {           
+            _balanceService = new Mock<IBalanceService>();
+        }
+
+        [Test]
+        public void CheckIfIsInsufficientFundsThrowsException()
+        {
+            _balanceService.Setup(s => s.IsInsufficientFunds(null, decimal.Zero)).Throws(new InvalidOperationException());
+        }
+
+        [Test]
+        public void CheckIfIsLowBalanceIsVerifiable()
+        {
+            _balanceService.Setup(s => s.IsLowBalance(null, decimal.Zero)).Verifiable();
+        }
+    }
+}
---
+++ src/Moneybox.Tests/Moneybox.Tests/Services/NotificationServiceTest.cs
@@ -0,0 +1,39 @@
+﻿using Moneybox.App;
+using Moneybox.App.Domain.Services;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace Moneybox.Tests.Services
+{
+    class NotificationServiceTest
+    {
+        private Mock<INotificationService> _notificationService;
+
+        [SetUp]
+        public void _SetUp()
+        {   
+            _notificationService = new Mock<INotificationService>();       
+        }
+
+        [Test]
+        public void GetNotification_GivenAccountIsNotNull()
+        {
+            var accountModel = new Account
+            {
+                Id = It.IsAny<Guid>(),
+                User = new User
+                {
+                    Id = It.IsAny<Guid>(),
+                    Name = "Joe",
+                    Email = "joe.bloggs@apple.com"
+                },
+                Balance = 500m,
+                Withdrawn = 200m,
+                PaidIn = 100m
+            };
+
+            _notificationService.Setup(s => s.NotifyFundsLow(accountModel.User.Email)).Verifiable();
+        }
+    }
+}
